144212f4a8e65ee620dcdd24ac0b5895a221e64d
paulscottrobson <paulscottrobson@gmail.com>
2019-01-07 18:10:18 +0000

    Converted to Windows build

--- kernel/temp/__words.asm
+++ kernel/temp/__words.asm
@@ -35,201 +35,6 @@ def_63_6f_6e_73_6f_6c_65_2e_69_6e_6b_65_79:
 ; ***************************************************************************************
 ; ***************************************************************************************
 ;
-;		Name : 		unary.asm
-;		Author :	Paul Robson (paul@robsons.org.uk)
-;		Date : 		5th January 2019
-;		Purpose :	Unary operators (A ? B -> A)
-;
-; ***************************************************************************************
-; ***************************************************************************************
-
-; ========= -- xmacro =========
-def_2d_2d:
-    call compileExecutableCopySelf
-start_2d_2d:
-    db end_2d_2d-start_2d_2d-1
-		dec 	hl
-end_2d_2d:
-    ret
-
-; ***************************************************************************************
-
-; ========= --- xmacro =========
-def_2d_2d_2d:
-    call compileExecutableCopySelf
-start_2d_2d_2d:
-    db end_2d_2d_2d-start_2d_2d_2d-1
-		dec 	hl
-		dec 	hl
-end_2d_2d_2d:
-    ret
-
-; ***************************************************************************************
-
-; ========= ++ xmacro =========
-def_2b_2b:
-    call compileExecutableCopySelf
-start_2b_2b:
-    db end_2b_2b-start_2b_2b-1
-		inc 	hl
-end_2b_2b:
-    ret
-
-; ***************************************************************************************
-
-; ========= +++ xmacro =========
-def_2b_2b_2b:
-    call compileExecutableCopySelf
-start_2b_2b_2b:
-    db end_2b_2b_2b-start_2b_2b_2b-1
-		inc 	hl
-		inc 	hl
-end_2b_2b_2b:
-    ret
-
-; ***************************************************************************************
-
-; ========= 0- word =========
-def_30_2d:
-    call compileCallToSelf
-__negate:
-		ld 		a,h
-		cpl
-		ld 		h,a
-		ld 		a,l
-		cpl
-		ld 		l,a
-		inc 	hl
-		ret
-
-; ***************************************************************************************
-
-; ========= 0< word =========
-def_30_3c:
-    call compileCallToSelf
-		bit 	7,h
-		ld 		hl,$0000
-		ret 	z
-		dec 	hl
-		ret
-
-; ***************************************************************************************
-
-; ========= 0= word =========
-def_30_3d:
-    call compileCallToSelf
-		ld 		a,h
-		or 		l
-		ld 		hl,$0000
-		ret 	nz
-		dec 	hl
-		ret
-
-; ***************************************************************************************
-
-; ========= 2* xmacro =========
-def_32_2a:
-    call compileExecutableCopySelf
-start_32_2a:
-    db end_32_2a-start_32_2a-1
-		add 	hl,hl
-end_32_2a:
-    ret
-
-; ========= 4* xmacro =========
-def_34_2a:
-    call compileExecutableCopySelf
-start_34_2a:
-    db end_34_2a-start_34_2a-1
-		add 	hl,hl
-		add 	hl,hl
-end_34_2a:
-    ret
-
-; ========= 8* xmacro =========
-def_38_2a:
-    call compileExecutableCopySelf
-start_38_2a:
-    db end_38_2a-start_38_2a-1
-		add 	hl,hl
-		add 	hl,hl
-		add 	hl,hl
-end_38_2a:
-    ret
-
-; ========= 16* xmacro =========
-def_31_36_2a:
-    call compileExecutableCopySelf
-start_31_36_2a:
-    db end_31_36_2a-start_31_36_2a-1
-		add 	hl,hl
-		add 	hl,hl
-		add 	hl,hl
-		add 	hl,hl
-end_31_36_2a:
-    ret
-
-; ***************************************************************************************
-
-; ========= 2/ xmacro =========
-def_32_2f:
-    call compileExecutableCopySelf
-start_32_2f:
-    db end_32_2f-start_32_2f-1
-		sra 	h
-		rr 		l
-end_32_2f:
-    ret
-
-; ========= 4/ xmacro =========
-def_34_2f:
-    call compileExecutableCopySelf
-start_34_2f:
-    db end_34_2f-start_34_2f-1
-		sra 	h
-		rr 		l
-		sra 	h
-		rr 		l
-end_34_2f:
-    ret
-
-; ***************************************************************************************
-
-; ========= abs word =========
-def_61_62_73:
-    call compileCallToSelf
-		bit 	7,h
-		ret		z
-		jp 		__negate
-
-; ***************************************************************************************
-
-; ========= bswap xmacro =========
-def_62_73_77_61_70:
-    call compileExecutableCopySelf
-start_62_73_77_61_70:
-    db end_62_73_77_61_70-start_62_73_77_61_70-1
-		ld 		a,l
-		ld 		l,h
-		ld 		h,a
-end_62_73_77_61_70:
-    ret
-
-; ***************************************************************************************
-
-; ========= not word =========
-def_6e_6f_74:
-    call compileCallToSelf
-		ld 		a,h
-		cpl
-		ld 		h,a
-		ld 		a,l
-		cpl
-		ld 		l,a
-		ret
-; ***************************************************************************************
-; ***************************************************************************************
-;
 ;		Name : 		binary.asm
 ;		Author :	Paul Robson (paul@robsons.org.uk)
 ;		Date : 		5th January 2019
@@ -370,138 +175,99 @@ def_78_6f_72:
 ; ***************************************************************************************
 ; ***************************************************************************************
 ;
-;		Name : 		stack.asm
+;		Name : 		memory.asm
 ;		Author :	Paul Robson (paul@robsons.org.uk)
 ;		Date : 		5th January 2019
-;		Purpose :	Stack words
+;		Purpose :	Memory operators
 ;
 ; ***************************************************************************************
 ; ***************************************************************************************
 
-; ========= push macro =========
-def_70_75_73_68:
-    call compileCopySelf
-start_70_75_73_68:
-    db end_70_75_73_68-start_70_75_73_68-1
-	push 	hl
-end_70_75_73_68:
-    ret
-
-; ========= pop macro =========
-def_70_6f_70:
-    call compileCopySelf
-start_70_6f_70:
-    db end_70_6f_70-start_70_6f_70-1
-	ex 		de,hl
-	pop 	hl
-end_70_6f_70:
+; ========= ! xmacro =========
+def_21:
+    call compileExecutableCopySelf
+start_21:
+    db end_21-start_21-1
+		ld 		(hl),e
+		inc 	hl
+		ld 		(hl),d
+		dec 	hl
+end_21:
     ret
 
 ; ***************************************************************************************
 
-; ========= a>r macro =========
-def_61_3e_72:
-    call compileCopySelf
-start_61_3e_72:
-    db end_61_3e_72-start_61_3e_72-1
-	push 	hl
-end_61_3e_72:
-    ret
-
-; ========= r>a macro =========
-def_72_3e_61:
-    call compileCopySelf
-start_72_3e_61:
-    db end_72_3e_61-start_72_3e_61-1
-	pop 	hl
-end_72_3e_61:
+; ========= @ xmacro =========
+def_40:
+    call compileExecutableCopySelf
+start_40:
+    db end_40-start_40-1
+		ld 		a,(hl)
+		inc 	hl
+		ld		h,(hl)
+		ld		l,a
+end_40:
     ret
 
 ; ***************************************************************************************
 
-; ========= b>r macro =========
-def_62_3e_72:
-    call compileCopySelf
-start_62_3e_72:
-    db end_62_3e_72-start_62_3e_72-1
-	push 	de
-end_62_3e_72:
-    ret
+; ========= +! word =========
+def_2b_21:
+    call compileCallToSelf
+		ld 		a,(hl)
+		add 	a,e
+		ld 		(hl),a
+		inc 	hl
+		ld 		a,(hl)
+		adc 	a,d
+		ld 		(hl),a
+		dec 	hl
+		ret
 
-; ========= r>b macro =========
-def_72_3e_62:
-    call compileCopySelf
-start_72_3e_62:
-    db end_72_3e_62-start_72_3e_62-1
-	pop 	de
-end_72_3e_62:
+; ***************************************************************************************
+
+; ========= c! xmacro =========
+def_63_21:
+    call compileExecutableCopySelf
+start_63_21:
+    db end_63_21-start_63_21-1
+		ld 		(hl),e
+end_63_21:
     ret
 
 ; ***************************************************************************************
 
-; ========= c>r macro =========
-def_63_3e_72:
-    call compileCopySelf
-start_63_3e_72:
-    db end_63_3e_72-start_63_3e_72-1
-	push 	bc
-end_63_3e_72:
-    ret
-
-; ========= r>c macro =========
-def_72_3e_63:
-    call compileCopySelf
-start_72_3e_63:
-    db end_72_3e_63-start_72_3e_63-1
-	pop 	bc
-end_72_3e_63:
-    ret
-
-
-; ***************************************************************************************
-
-; ========= ab>r macro =========
-def_61_62_3e_72:
-    call compileCopySelf
-start_61_62_3e_72:
-    db end_61_62_3e_72-start_61_62_3e_72-1
-	push 	de
-	push 	hl
-end_61_62_3e_72:
-    ret
-
-; ========= r>ab macro =========
-def_72_3e_61_62:
-    call compileCopySelf
-start_72_3e_61_62:
-    db end_72_3e_61_62-start_72_3e_61_62-1
-	pop 	hl
-	pop 	de
-end_72_3e_61_62:
+; ========= c@ xmacro =========
+def_63_40:
+    call compileExecutableCopySelf
+start_63_40:
+    db end_63_40-start_63_40-1
+		ld 		l,(hl)
+		ld 		h,0
+end_63_40:
     ret
 
 ; ***************************************************************************************
 
-; ========= abc>r macro =========
-def_61_62_63_3e_72:
-    call compileCopySelf
-start_61_62_63_3e_72:
-    db end_61_62_63_3e_72-start_61_62_63_3e_72-1
-	push 	bc
-	push 	de
-	push 	hl
-end_61_62_63_3e_72:
+; ========= p@ xmacro =========
+def_70_40:
+    call compileExecutableCopySelf
+start_70_40:
+    db end_70_40-start_70_40-1
+		in 		l,(c)
+		ld 		h,0
+end_70_40:
     ret
 
-; ========= r>abc macro =========
-def_72_3e_61_62_63:
+; ***************************************************************************************
+
+; ========= p! macro =========
+def_70_21:
     call compileCopySelf
-start_72_3e_61_62_63:
-    db end_72_3e_61_62_63-start_72_3e_61_62_63-1
-	pop 	hl
-	pop 	de
-	pop 	bc
-end_72_3e_61_62_63:
+start_70_21:
+    db end_70_21-start_70_21-1
+		out 	(c),l
+end_70_21:
     ret
 ; ***************************************************************************************
 ; ***************************************************************************************
@@ -778,97 +544,331 @@ end_63_3e_62:
 ; ***************************************************************************************
 ; ***************************************************************************************
 ;
-;		Name : 		memory.asm
+;		Name : 		stack.asm
 ;		Author :	Paul Robson (paul@robsons.org.uk)
 ;		Date : 		5th January 2019
-;		Purpose :	Memory operators
+;		Purpose :	Stack words
 ;
 ; ***************************************************************************************
 ; ***************************************************************************************
 
-; ========= ! xmacro =========
-def_21:
+; ========= push macro =========
+def_70_75_73_68:
+    call compileCopySelf
+start_70_75_73_68:
+    db end_70_75_73_68-start_70_75_73_68-1
+	push 	hl
+end_70_75_73_68:
+    ret
+
+; ========= pop macro =========
+def_70_6f_70:
+    call compileCopySelf
+start_70_6f_70:
+    db end_70_6f_70-start_70_6f_70-1
+	ex 		de,hl
+	pop 	hl
+end_70_6f_70:
+    ret
+
+; ***************************************************************************************
+
+; ========= a>r macro =========
+def_61_3e_72:
+    call compileCopySelf
+start_61_3e_72:
+    db end_61_3e_72-start_61_3e_72-1
+	push 	hl
+end_61_3e_72:
+    ret
+
+; ========= r>a macro =========
+def_72_3e_61:
+    call compileCopySelf
+start_72_3e_61:
+    db end_72_3e_61-start_72_3e_61-1
+	pop 	hl
+end_72_3e_61:
+    ret
+
+; ***************************************************************************************
+
+; ========= b>r macro =========
+def_62_3e_72:
+    call compileCopySelf
+start_62_3e_72:
+    db end_62_3e_72-start_62_3e_72-1
+	push 	de
+end_62_3e_72:
+    ret
+
+; ========= r>b macro =========
+def_72_3e_62:
+    call compileCopySelf
+start_72_3e_62:
+    db end_72_3e_62-start_72_3e_62-1
+	pop 	de
+end_72_3e_62:
+    ret
+
+; ***************************************************************************************
+
+; ========= c>r macro =========
+def_63_3e_72:
+    call compileCopySelf
+start_63_3e_72:
+    db end_63_3e_72-start_63_3e_72-1
+	push 	bc
+end_63_3e_72:
+    ret
+
+; ========= r>c macro =========
+def_72_3e_63:
+    call compileCopySelf
+start_72_3e_63:
+    db end_72_3e_63-start_72_3e_63-1
+	pop 	bc
+end_72_3e_63:
+    ret
+
+
+; ***************************************************************************************
+
+; ========= ab>r macro =========
+def_61_62_3e_72:
+    call compileCopySelf
+start_61_62_3e_72:
+    db end_61_62_3e_72-start_61_62_3e_72-1
+	push 	de
+	push 	hl
+end_61_62_3e_72:
+    ret
+
+; ========= r>ab macro =========
+def_72_3e_61_62:
+    call compileCopySelf
+start_72_3e_61_62:
+    db end_72_3e_61_62-start_72_3e_61_62-1
+	pop 	hl
+	pop 	de
+end_72_3e_61_62:
+    ret
+
+; ***************************************************************************************
+
+; ========= abc>r macro =========
+def_61_62_63_3e_72:
+    call compileCopySelf
+start_61_62_63_3e_72:
+    db end_61_62_63_3e_72-start_61_62_63_3e_72-1
+	push 	bc
+	push 	de
+	push 	hl
+end_61_62_63_3e_72:
+    ret
+
+; ========= r>abc macro =========
+def_72_3e_61_62_63:
+    call compileCopySelf
+start_72_3e_61_62_63:
+    db end_72_3e_61_62_63-start_72_3e_61_62_63-1
+	pop 	hl
+	pop 	de
+	pop 	bc
+end_72_3e_61_62_63:
+    ret
+; ***************************************************************************************
+; ***************************************************************************************
+;
+;		Name : 		unary.asm
+;		Author :	Paul Robson (paul@robsons.org.uk)
+;		Date : 		5th January 2019
+;		Purpose :	Unary operators (A ? B -> A)
+;
+; ***************************************************************************************
+; ***************************************************************************************
+
+; ========= -- xmacro =========
+def_2d_2d:
     call compileExecutableCopySelf
-start_21:
-    db end_21-start_21-1
-		ld 		(hl),e
-		inc 	hl
-		ld 		(hl),d
+start_2d_2d:
+    db end_2d_2d-start_2d_2d-1
 		dec 	hl
-end_21:
+end_2d_2d:
     ret
 
 ; ***************************************************************************************
 
-; ========= @ xmacro =========
-def_40:
+; ========= --- xmacro =========
+def_2d_2d_2d:
     call compileExecutableCopySelf
-start_40:
-    db end_40-start_40-1
-		ld 		a,(hl)
-		inc 	hl
-		ld		h,(hl)
-		ld		l,a
-end_40:
+start_2d_2d_2d:
+    db end_2d_2d_2d-start_2d_2d_2d-1
+		dec 	hl
+		dec 	hl
+end_2d_2d_2d:
     ret
 
 ; ***************************************************************************************
 
-; ========= +! word =========
-def_2b_21:
-    call compileCallToSelf
-		ld 		a,(hl)
-		add 	a,e
-		ld 		(hl),a
+; ========= ++ xmacro =========
+def_2b_2b:
+    call compileExecutableCopySelf
+start_2b_2b:
+    db end_2b_2b-start_2b_2b-1
 		inc 	hl
-		ld 		a,(hl)
-		adc 	a,d
-		ld 		(hl),a
+end_2b_2b:
+    ret
+
+; ***************************************************************************************
+
+; ========= +++ xmacro =========
+def_2b_2b_2b:
+    call compileExecutableCopySelf
+start_2b_2b_2b:
+    db end_2b_2b_2b-start_2b_2b_2b-1
+		inc 	hl
+		inc 	hl
+end_2b_2b_2b:
+    ret
+
+; ***************************************************************************************
+
+; ========= 0- word =========
+def_30_2d:
+    call compileCallToSelf
+__negate:
+		ld 		a,h
+		cpl
+		ld 		h,a
+		ld 		a,l
+		cpl
+		ld 		l,a
+		inc 	hl
+		ret
+
+; ***************************************************************************************
+
+; ========= 0< word =========
+def_30_3c:
+    call compileCallToSelf
+		bit 	7,h
+		ld 		hl,$0000
+		ret 	z
 		dec 	hl
 		ret
 
 ; ***************************************************************************************
 
-; ========= c! xmacro =========
-def_63_21:
+; ========= 0= word =========
+def_30_3d:
+    call compileCallToSelf
+		ld 		a,h
+		or 		l
+		ld 		hl,$0000
+		ret 	nz
+		dec 	hl
+		ret
+
+; ***************************************************************************************
+
+; ========= 2* xmacro =========
+def_32_2a:
     call compileExecutableCopySelf
-start_63_21:
-    db end_63_21-start_63_21-1
-		ld 		(hl),e
-end_63_21:
+start_32_2a:
+    db end_32_2a-start_32_2a-1
+		add 	hl,hl
+end_32_2a:
+    ret
+
+; ========= 4* xmacro =========
+def_34_2a:
+    call compileExecutableCopySelf
+start_34_2a:
+    db end_34_2a-start_34_2a-1
+		add 	hl,hl
+		add 	hl,hl
+end_34_2a:
+    ret
+
+; ========= 8* xmacro =========
+def_38_2a:
+    call compileExecutableCopySelf
+start_38_2a:
+    db end_38_2a-start_38_2a-1
+		add 	hl,hl
+		add 	hl,hl
+		add 	hl,hl
+end_38_2a:
+    ret
+
+; ========= 16* xmacro =========
+def_31_36_2a:
+    call compileExecutableCopySelf
+start_31_36_2a:
+    db end_31_36_2a-start_31_36_2a-1
+		add 	hl,hl
+		add 	hl,hl
+		add 	hl,hl
+		add 	hl,hl
+end_31_36_2a:
     ret
 
 ; ***************************************************************************************
 
-; ========= c@ xmacro =========
-def_63_40:
+; ========= 2/ xmacro =========
+def_32_2f:
     call compileExecutableCopySelf
-start_63_40:
-    db end_63_40-start_63_40-1
-		ld 		l,(hl)
-		ld 		h,0
-end_63_40:
+start_32_2f:
+    db end_32_2f-start_32_2f-1
+		sra 	h
+		rr 		l
+end_32_2f:
+    ret
+
+; ========= 4/ xmacro =========
+def_34_2f:
+    call compileExecutableCopySelf
+start_34_2f:
+    db end_34_2f-start_34_2f-1
+		sra 	h
+		rr 		l
+		sra 	h
+		rr 		l
+end_34_2f:
     ret
 
 ; ***************************************************************************************
 
-; ========= p@ xmacro =========
-def_70_40:
+; ========= abs word =========
+def_61_62_73:
+    call compileCallToSelf
+		bit 	7,h
+		ret		z
+		jp 		__negate
+
+; ***************************************************************************************
+
+; ========= bswap xmacro =========
+def_62_73_77_61_70:
     call compileExecutableCopySelf
-start_70_40:
-    db end_70_40-start_70_40-1
-		in 		l,(c)
-		ld 		h,0
-end_70_40:
+start_62_73_77_61_70:
+    db end_62_73_77_61_70-start_62_73_77_61_70-1
+		ld 		a,l
+		ld 		l,h
+		ld 		h,a
+end_62_73_77_61_70:
     ret
 
 ; ***************************************************************************************
 
-; ========= p! macro =========
-def_70_21:
-    call compileCopySelf
-start_70_21:
-    db end_70_21-start_70_21-1
-		out 	(c),l
-end_70_21:
-    ret
+; ========= not word =========
+def_6e_6f_74:
+    call compileCallToSelf
+		ld 		a,h
+		cpl
+		ld 		h,a
+		ld 		a,l
+		cpl
+		ld 		l,a
+		ret
